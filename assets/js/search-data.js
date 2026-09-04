18c1a5643954297a72e54e02ce9d520a04c2e854
timetzw <timetzw@outlook.com>
2026-09-04 19:44:12 +0000

    Deploying to gh-pages from @ timetzw/timetzw.github.io@b0f95cf97e94e960b28e34ebddbaeb3e3e6467e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,64 +11,82 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "My research sorted by year.",
+          description: "Publications by Zhengwei Tong in cryptography, decentralized systems, AI evaluation, and optimization.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-i-ve-started-my-student-researcher-internship-at-solana-foundation",
-          title: 'I’ve started my Student Researcher internship at Solana Foundation.',
+        },{id: "news-i-ve-started-my-student-researcher-internship-at-solana-foundation",
+          title: "I’ve started my Student Researcher internship at Solana Foundation.",
           description: "",
-          section: "News",},{id: "news-we-presented-our-hackathon-project-persisting-private-bittorrent-trackers-with-tees-at-the-ic3-blockchain-summer-camp-2025-with-françois-xavier-wicht-aviv-yaish-hang-yin-and-andrew-miller",
-          title: 'We presented our hackathon project Persisting Private BitTorrent Trackers with TEEs at the...',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-we-presented-our-hackathon-project-persisting-private-bittorrent-trackers-with-tees-at-the-ic3-blockchain-summer-camp-2025-with-françois-xavier-wicht-aviv-yaish-hang-yin-and-andrew-miller",
+          title: "We presented our hackathon project Persisting Private BitTorrent Trackers with TEEs at the IC3 Blockchain Summer Camp 2025 with François-Xavier...",
           description: "",
-          section: "News",},{id: "news-i-will-be-presenting-privacy-preserving-inclusion-lists-at-taps-at-ncsu",
-          title: 'I will be presenting Privacy-Preserving Inclusion Lists  at TAPS at NCSU.',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-i-presented-privacy-preserving-inclusion-lists-at-taps-at-ncsu",
+          title: "I presented Privacy-Preserving Inclusion Lists at TAPS at NCSU.",
           description: "",
-          section: "News",},{id: "news-our-paper-persistent-bittorrent-trackers-has-been-accepted-at-ieee-euro-s-amp-amp-p-2026",
-          title: 'Our paper Persistent BitTorrent Trackers has been accepted at IEEE Euro S&amp;amp;amp;P 2026....',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-our-paper-persistent-bittorrent-trackers-has-been-accepted-at-ieee-euro-s-amp-amp-p-2026",
+          title: "Our paper Persistent BitTorrent Trackers has been accepted at IEEE Euro S\u0026P 2026.",
           description: "",
-          section: "News",},{id: "news-i-passed-my-preliminary-exam-and-am-now-a-phd-candidate",
-          title: 'I passed my preliminary exam and am now a PhD candidate!',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-i-passed-my-preliminary-exam-and-am-now-a-phd-candidate",
+          title: "I passed my preliminary exam and am now a PhD candidate!",
           description: "",
-          section: "News",},{id: "news-our-paper-persistent-bittorrent-trackers-has-also-been-accepted-at-sbc-2026",
-          title: 'Our paper Persistent BitTorrent Trackers has also been accepted at SBC 2026!',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-our-paper-persistent-bittorrent-trackers-has-also-been-accepted-at-sbc-2026",
+          title: "Our paper Persistent BitTorrent Trackers has also been accepted at SBC 2026!",
           description: "",
-          section: "News",},{id: "news-i-presented-our-work-persistent-bittorrent-trackers-at-the-ic3-blockchain-summer-camp-2026",
-          title: 'I presented our work Persistent BitTorrent Trackers at the IC3 Blockchain Summer Camp...',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-i-presented-our-work-persistent-bittorrent-trackers-at-the-ic3-blockchain-summer-camp-2026",
+          title: "I presented our work Persistent BitTorrent Trackers at the IC3 Blockchain Summer Camp 2026.",
           description: "",
-          section: "News",},{id: "news-i-attended-the-casa-summer-school-2026-on-cryptography-and-distributed-computing-in-bochum-germany",
-          title: 'I attended the CASA Summer School 2026 on Cryptography and Distributed Computing in...',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-i-attended-the-casa-summer-school-2026-on-cryptography-and-distributed-computing-in-bochum-germany",
+          title: "I attended the CASA Summer School 2026 on Cryptography and Distributed Computing in Bochum, Germany.",
           description: "",
-          section: "News",},{id: "news-our-paper-privacy-preserving-inclusion-lists-has-been-accepted-at-aft-2026",
-          title: 'Our paper Privacy-Preserving Inclusion Lists has been accepted at AFT 2026!',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-our-paper-privacy-preserving-inclusion-lists-has-been-accepted-at-aft-2026",
+          title: "Our paper Privacy-Preserving Inclusion Lists has been accepted at AFT 2026!",
           description: "",
-          section: "News",},{id: "news-i-ve-started-my-internship-at-nethermind",
-          title: 'I’ve started my internship at Nethermind.',
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{id: "news-i-ve-started-my-internship-at-nethermind",
+          title: "I’ve started my internship at Nethermind.",
           description: "",
-          section: "News",},{
+          section: "News",
+            handler: () => {
+              window.location.href = "/news/";
+            },
+        },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -101,7 +119,7 @@ ninja.data = [{
         title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://twitter.com/Zhengwei_Tong# your X handle", "_blank");
+          window.open("https://twitter.com/Zhengwei_Tong", "_blank");
         },
       },{
       id: 'light-theme',
